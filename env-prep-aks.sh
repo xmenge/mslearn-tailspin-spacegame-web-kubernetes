a42a402e48c7638d1315556a51722e8a4a10146b
gl <39395994+xmenge@users.noreply.github.com>
2020-05-11 22:21:46 +1000

    add env prep script

---
+++ env-prep-aks.sh
@@ -0,0 +1,54 @@
+az configure --defaults location=australiaeast
+
+resourceSuffix=000
+
+registryName="tsgaksacr${resourceSuffix}"
+aksName="tsg-k8s-${resourceSuffix}"
+rgName='tsg-aks-rg'
+
+aksVersion=$(az aks get-versions \
+  --query 'orchestrators[-1].orchestratorVersion' \
+  --output tsv)
+
+az group create --name $rgName
+
+az acr create \
+  --name $registryName \
+  --resource-group $rgName \
+  --sku Standard
+
+
+az aks create \
+  --name $aksName \
+  --resource-group $rgName \
+  --enable-addons monitoring \
+  --kubernetes-version $aksVersion \
+  --generate-ssh-keys
+
+
+clientId=$(az aks show \
+  --name $aksName \
+  --resource-group $rgName \
+  --query "servicePrincipalProfile.clientId" \
+  --output tsv)
+
+
+acrId=$(az acr show \
+  --name $registryName \
+  --resource-group $rgName \
+  --query "id" \
+  --output tsv)
+
+
+az acr list \
+ --resource-group $rgName \
+ --query "[].{loginServer: loginServer}" \
+ --output table
+
+
+az role assignment create \
+  --assignee $clientId \
+  --role acrpull \
+  --scope $acrId
+
+  
